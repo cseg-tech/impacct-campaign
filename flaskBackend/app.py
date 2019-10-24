6c840530c45b8efee37dd29c1fee9bd549402c4c
Mustafa Eyceoz <me2680@columbia.edu>
2019-10-24 04:10:55 -0400

    REACT APP WORKING WITH FLASK BACKEND
    woooooooooooooo

--- flaskBackend/app.py
+++ flaskBackend/app.py
@@ -1,11 +1,14 @@
 
 from flask import Flask, jsonify
+from flask_cors import CORS
+import sys
+
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/api/simplelist', methods=['GET'])
 def hello():
     return jsonify({'Item 1':'chicken', 'Item 2':'steak', 'Item 3':'wegetarian for Raghav'})
 
-
-
+print(sys.path)
---
+++ react-test/src/App.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      message: "Hello World",
+      data: null
+    }
+  }
+
+  fetchData = () => {
+
+    fetch('http://localhost:5000/api/simplelist')
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
+
+      console.log(this.state.data);
+    //fetch('http://localhost:5000/api/simplelist').then(response => response.json()).then(data =>console.log(response))
+      // Do something with response
+
+    /*
+    fetch('',{mode: 'no-cors'})
+        .then(response => response.json())
+        .then(data => console.log(data));
+    */
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.message}
+          </p>
+          <button onClick={this.fetchData}>Click me</button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
