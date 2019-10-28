5c03c8c158859f09ae5f29bb409323f82e888e7c
Angela Zhang <angela.zhang043@gmail.com>
2019-10-27 22:49:38 -0400

    tranferred html pages to react
    further work still needs to be done but this is the status as of now

---
+++ react-test/src/complexCampaign.js
@@ -0,0 +1,45 @@
+//contact.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ComplexCampaign extends React.Component {
+ constructor(props) {
+    super(props);
+    this.state = {
+      result: 0,
+      num1: 0,
+      num2: 0
+    };
+    this.handlenum1Change = this.handlenum1Change.bind(this);
+    this.handlenum2Change = this.handlenum2Change.bind(this);
+  }
+  handlenum1Change (evt) {
+    console.log(evt.target.value);
+    this.setState({ num1: Number(evt.target.value) });
+  }
+  handlenum2Change(evt) {
+    console.log(typeof evt.target.value);
+    this.setState({ num2: Number(evt.target.value) });
+  }
+  addAction =(event)=> {
+    let x = this.state.num1 + this.state.num2
+    this.setState({result: x })
+  }
+render() {
+ return (
+    <form>
+        <label>
+          <input type="number" onChange={this.handlenum1Change} />
+          <input type="number"   onChange={this.handlenum2Change}/>
+          <input type="button" onClick={this.addAction} value="Add"/>
+          <input type='text' value={this.state.result} readOnly/>
+        </label>
+      </form>
+    );
+  }
+}
+
+
+
+
+export default ComplexCampaign;
