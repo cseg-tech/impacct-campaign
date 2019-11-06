8ab783f5f082d3e1681290d18fda9a49220c8e2d
Mustafa Eyceoz <me2680@columbia.edu>
2019-11-06 16:12:28 -0500

    Update App.js
    Added a simple .then

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -16,9 +16,12 @@ class App extends Component {
 
     fetch('http://impacct-test.herokuapp.com/getall')
       .then(response => response.json())
-      .then(data => this.setState({ data }));
+      .then(data => {
+        this.setState({ data });
+        console.log(this.state.data);
+      });
 
-      console.log(this.state.data);
+      // console.log(this.state.data);
     //fetch('http://localhost:5000/api/simplelist').then(response => response.json()).then(data =>console.log(response))
       // Do something with response
 
