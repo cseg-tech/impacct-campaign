e09668a894d0aded82578e67dfd4c69888bd0b2c
jjlee0802cu <jjlee0802@gmail.com>
2020-01-21 23:26:57 -0500

    Kanav's server changes

--- flaskApp/__init__.py
+++ flaskApp/__init__.py
@@ -1,7 +1,51 @@
 from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import sys
 
 application = Flask(__name__, static_folder="./static/dist", template_folder="./static")
+application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://nycdb:nycdb@127.0.0.1/nycdb'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(application)
+
+#from models import LL7_qualified
+
+class LL7_qualified(db.Model):
+    __tablename__ = 'll7_qualified'
+    borough = db.Column(db.String())
+    boro = db.Column(db.Integer)
+    block = db.Column(db.Integer)
+    lot = db.Column(db.Integer)
+    bbl = db.Column(db.BigInteger, primary_key = True)
+    hnumlo = db.Column(db.Integer)
+    hnumhi = db.Column(db.Integer)
+    strname = db.Column(db.String())
+    crfn = db.Column(db.BigInteger)
+    grantee = db.Column(db.String())
+    deeddate = db.Column(db.Date)
+    price = db.Column(db.Integer)
+    caprate = db.Column(db.Float)
+    boroughcaprate = db.Column(db.Float)
+    yearqtr = db.Column(db.String())
+    postcode = db.Column(db.Integer)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    communityboard = db.Column(db.Integer)
+    councildistrict = db.Column(db.Integer)
+    censustract = db.Column(db.Integer)
+    bin = db.Column(db.Integer)
+    nta = db.Column(db.String())
+    location1 = db.Column(db.String())
+
+    def serialize(self):
+        return {
+            'street address' : str(self.hnumlo) + " " + self.strname,
+            'zipcode' : str(self.postcode),
+            'borough' : str(self.boro),
+            'bbl' : str(self.bbl)
+        }
+
+    def __repr__(self):
+        return str(self.bbl)
 
 def sendData():
     data = request.get_json(force=True)
@@ -17,7 +61,12 @@ def nyattorneySearch():
 
 @application.route("/api/speculationWatchlistSearch", methods=['POST'])
 def speculationWatchlistSearch():
-	return sendData()
+    data = request.get_json(force=True)
+    zipcode = data['zipcode']
+    borough = data['borough']
+    buildings = LL7_qualified.query.filter_by(postcode=zipcode).filter_by(boro=borough)
+
+    return jsonify(list(map(lambda x:x.serialize(), buildings)))
 
 @application.route("/api/vacateOrdersSearch", methods=['POST'])
 def vacateOrdersSearch():
