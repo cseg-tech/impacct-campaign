20c794006a17e130dd462813feccd948b6b47aa7
jjlee0802cu <jjlee0802@gmail.com>
2020-01-23 18:13:45 -0500

    This is still Musti, also here is table format for spec watchlist

--- flaskApp/static/js/speculationWatchlist.js
+++ flaskApp/static/js/speculationWatchlist.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { JsonToTable } from "react-json-to-table";
 
 class speculationWatchlist extends React.Component {
   constructor() {
@@ -71,7 +72,7 @@ class speculationWatchlist extends React.Component {
         {
           serverData ? 
           serverData.map((data) => (
-            <p>{JSON.stringify(data)}</p>
+            <p> <JsonToTable json={data} /> </p>
           ))
           :
           (
